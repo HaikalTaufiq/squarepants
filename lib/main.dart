aa574e16183bedcc6be1b649bad6a5fb76ed0a24
M Taufiq Karim Haikal <134383454+HaikalTaufiq@users.noreply.github.com>
2024-08-15 15:38:09 -1200

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,10 @@
-// ignore_for_file: prefer_const_constructors
+// ignore_for_file: prefer_const_constructors_in_immutables
 
-import 'package:firebase_auth/firebase_auth.dart';
-import 'package:firebase_core/firebase_core.dart';
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
-import 'package:square_pants/pages/home_page.dart';
-import 'package:square_pants/service/notif_service.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:Squarepants/pages/home_page.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:Squarepants/service/notif_service.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -24,53 +22,31 @@ class MainApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: LoginPage(),
+      initialRoute: '/',
+      routes: {
+        '/login': (context) => LoginPage(),
+      },
     );
   }
 }
 
-class LoginPage extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Color(0xff2A9DAF),
-      body: Container(
-        decoration: BoxDecoration(
-          image: DecorationImage(
-              image: AssetImage('assets/images/login.png'),
-              fit: BoxFit.contain,
-              alignment: Alignment.topCenter // Adjust as needed
-              ),
-        ),
-        child: Center(
-          child: Column(
-            children: [
-              Column(
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: [],
-              ),
-              AuthenticationForm(),
-            ],
-          ),
-        ),
-      ),
-    );
-  }
-}
-
-class AuthenticationForm extends StatefulWidget {
-  const AuthenticationForm({Key? key}) : super(key: key);
+class LoginPage extends StatefulWidget {
+  LoginPage({super.key});
 
   @override
-  _AuthenticationFormState createState() => _AuthenticationFormState();
+  State<LoginPage> createState() => _LoginPageState();
 }
 
-class _AuthenticationFormState extends State<AuthenticationForm> {
+class _LoginPageState extends State<LoginPage> {
   final FirebaseAuth _auth = FirebaseAuth.instance;
+
   final TextEditingController _emailController = TextEditingController();
+
   final TextEditingController _passwordController = TextEditingController();
+
   String _errorMessage = '';
 
-  void _signInWithEmailAndPassword() async {
+  void _signInWithEmailAndPassword(BuildContext context) async {
     try {
       final UserCredential userCredential =
           await _auth.signInWithEmailAndPassword(
@@ -79,9 +55,7 @@ class _AuthenticationFormState extends State<AuthenticationForm> {
       );
       // Check if the userCredential is not null (successful login)
       if (userCredential.user != null) {
-        setState(() {
-          _errorMessage = '';
-        });
+        _errorMessage = '';
         print('User logged in successfully: ${userCredential.user!.uid}');
         // Navigate to another screen or perform any other actions after login
         Navigator.push(
@@ -93,7 +67,7 @@ class _AuthenticationFormState extends State<AuthenticationForm> {
       }
     } catch (e) {
       setState(() {
-        _errorMessage = e.toString();
+        _errorMessage = 'Invalid username or password. Please try again.';
       });
       print('Failed to sign in with Email & Password: $_errorMessage');
     }
@@ -101,139 +75,156 @@ class _AuthenticationFormState extends State<AuthenticationForm> {
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.only(top: 272),
-      child: Container(
-        decoration: BoxDecoration(
-            color: Color(0xffffffff),
-            borderRadius: BorderRadius.only(
-                topLeft: Radius.circular(24), topRight: Radius.circular(24))),
-        child: Container(
-          height: 510,
-          child: Column(
-            mainAxisSize: MainAxisSize.min,
-            mainAxisAlignment: MainAxisAlignment.start,
-            children: <Widget>[
-              Padding(
-                padding: const EdgeInsets.only(top: 30, bottom: 10),
-                child: Transform.scale(
-                    scale: 1.0, child: Image.asset('assets/images/sqp.png')),
-              ),
-              Padding(
-                padding: const EdgeInsets.only(bottom: 30),
-                child: Text(
-                  'Hello Fish Owner',
-                  style: TextStyle(
-                    fontFamily: 'poppins',
-                    fontSize: 16,
-                    fontWeight: FontWeight.w600,
+    return Scaffold(
+      backgroundColor: Color(0xffffffffF),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Stack(
+              children: [
+                Transform.scale(
+                  scale: 1.2,
+                  child: Image.asset('assets/images/76.png'),
+                ),
+                Transform.translate(
+                  offset: Offset(0, -100),
+                  child: Container(
+                    child: Transform.scale(
+                      scale: 0.3,
+                      child: Image.asset('assets/images/w1.png'),
+                    ),
                   ),
                 ),
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.start,
-                children: [
-                  Padding(
-                    padding: const EdgeInsets.only(
-                      left: 42,
-                    ),
-                    child: Text(
-                      'Account',
-                      style: TextStyle(
-                        fontFamily: 'poppins',
-                        fontSize: 16,
-                        fontWeight: FontWeight.w500,
+              ],
+            ),
+            Transform.translate(
+              offset: Offset(0, -130),
+              child: Padding(
+                padding: const EdgeInsets.only(left: 35, right: 35),
+                child: Container(
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(15),
+                  ),
+                  child: Column(
+                    mainAxisSize: MainAxisSize.min,
+                    crossAxisAlignment: CrossAxisAlignment.stretch,
+                    children: [
+                      Text(
+                        'Welcome Fish Owner!',
+                        textAlign: TextAlign.center,
+                        style: TextStyle(
+                          fontFamily: 'poppins',
+                          fontSize: 22,
+                          fontWeight: FontWeight.w700,
+                        ),
                       ),
-                    ),
-                  ),
-                ],
-              ),
-              Container(
-                width: 320,
-                height: 57,
-                margin: EdgeInsets.only(top: 10),
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.all(Radius.circular(14)),
-                  color: Color(0xffEBFDFC),
-                ),
-                child: TextField(
-                  controller: _emailController,
-                  decoration: InputDecoration(
-                    border: InputBorder.none,
-                    contentPadding:
-                        EdgeInsets.symmetric(horizontal: 20, vertical: 20),
-                  ),
-                ),
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.start,
-                children: [
-                  Padding(
-                    padding: const EdgeInsets.only(left: 40, top: 10),
-                    child: Text(
-                      'Password',
-                      style: TextStyle(
-                        fontFamily: 'poppins',
-                        fontSize: 16,
-                        fontWeight: FontWeight.w500,
+                      SizedBox(height: 20),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        children: [
+                          Padding(
+                            padding: const EdgeInsets.only(left: 8),
+                            child: Text(
+                              'Account',
+                              style: TextStyle(
+                                fontFamily: 'poppins',
+                                fontSize: 16,
+                                fontWeight: FontWeight.w500,
+                              ),
+                            ),
+                          ),
+                        ],
                       ),
-                    ),
+                      Container(
+                        width: 320,
+                        height: 57,
+                        margin: EdgeInsets.only(top: 10),
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.all(Radius.circular(14)),
+                          color: Color(0xffEBFDFC),
+                        ),
+                        child: TextField(
+                          controller: _emailController,
+                          decoration: InputDecoration(
+                            labelStyle: TextStyle(fontFamily: 'poppins'),
+                            border: InputBorder.none,
+                            contentPadding: EdgeInsets.symmetric(
+                                horizontal: 20, vertical: 20),
+                          ),
+                        ),
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        children: [
+                          Padding(
+                            padding: const EdgeInsets.only(left: 8, top: 10),
+                            child: Text(
+                              'Password',
+                              style: TextStyle(
+                                fontFamily: 'poppins',
+                                fontSize: 16,
+                                fontWeight: FontWeight.w500,
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                      Container(
+                        width: 320,
+                        height: 57,
+                        margin: EdgeInsets.only(top: 10),
+                        decoration: BoxDecoration(
+                          color: Color(0xffEBFDFC),
+                          borderRadius: BorderRadius.all(Radius.circular(14)),
+                        ),
+                        child: TextField(
+                          controller: _passwordController,
+                          obscureText: true,
+                          decoration: InputDecoration(
+                            border: InputBorder.none,
+                            contentPadding: EdgeInsets.symmetric(
+                                horizontal: 20, vertical: 20),
+                          ),
+                        ),
+                      ),
+                      SizedBox(height: 25),
+                      ElevatedButton(
+                        onPressed: () => _signInWithEmailAndPassword(context),
+                        style: ElevatedButton.styleFrom(
+                          backgroundColor: Color(0xFF25A1AE).withOpacity(0.7),
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(10),
+                          ),
+                          padding: EdgeInsets.symmetric(vertical: 15),
+                        ),
+                        child: Text(
+                          'Log In',
+                          style: TextStyle(
+                            fontFamily: "poppins",
+                            color: Colors.white,
+                            fontSize: 18,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                      ),
+                      if (_errorMessage.isNotEmpty)
+                        Padding(
+                          padding: EdgeInsets.symmetric(vertical: 10),
+                          child: Text(
+                            _errorMessage,
+                            textAlign: TextAlign.center,
+                            style: TextStyle(color: Colors.red),
+                          ),
+                        ),
+                    ],
                   ),
-                ],
+                ),
               ),
-              Container(
-                width: 320,
-                height: 57,
-                margin: EdgeInsets.only(top: 10),
-                decoration: BoxDecoration(
-                  color: Color(0xffEBFDFC),
-                  borderRadius: BorderRadius.all(Radius.circular(14)),
-                ),
-                child: TextField(
-                  controller: _passwordController,
-                  obscureText: true,
-                  decoration: InputDecoration(
-                    border: InputBorder.none,
-                    contentPadding:
-                        EdgeInsets.symmetric(horizontal: 20, vertical: 20),
-                  ),
-                ),
-              ),
-              SizedBox(height: 16.0),
-              Container(
-                width: 315,
-                child: FloatingActionButton(
-                  backgroundColor: Color(0xFF25A1AE).withOpacity(0.7),
-                  onPressed: _signInWithEmailAndPassword,
-                  child: Text(
-                    'Log In',
-                    style: TextStyle(
-                        fontFamily: "poppins",
-                        color: Colors.white,
-                        fontSize: 18,
-                        fontWeight: FontWeight.bold),
-                  ),
-                ),
-              ),
-              if (_errorMessage.isNotEmpty)
-                Padding(
-                  padding: const EdgeInsets.only(top: 10),
-                  child: Text(
-                    _errorMessage,
-                    style: TextStyle(color: Colors.red),
-                  ),
-                ),
-            ],
-          ),
+            ),
+          ],
         ),
       ),
     );
-  }
-
-  @override
-  void dispose() {
-    _emailController.dispose();
-    _passwordController.dispose();
-    super.dispose();
   }
 }
